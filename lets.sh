5d2c2f10b6611a05a54d65ac55dd21c7f7727cd1
Lucas Fernandes <lucas.fernandes@fabrica.locaweb.com.br>
2022-12-05 12:05:35 -0300

    Correção na verificação da entrada www

--- lets.sh
+++ lets.sh
@@ -16,16 +16,16 @@ echo "Insira o dominio: "
 read dominio
 mkdir .acme.sh
 # Verifica se o site tem extensão www ou não 
-ping -q -c5 www.$dominio > /dev/null
-if [$? -eq 0]
+ping -q -c2 www.$dominio > /dev/null
+if [ $? -eq 0 ]
 then
-	git clone https://github.com/Neilpang/acme.sh.git
-	sh acme.sh/acme.sh --set-default-ca --server letsencrypt
-	sh acme.sh/acme.sh --issue -d $dominio -d www.$dominio -w /home/$ftpuser/public_html --server https://acme-v02.api.letsencrypt.org/directory
-else 
-	git clone https://github.com/Neilpang/acme.sh.git
-	sh acme.sh/acme.sh --set-default-ca --server letsencrypt
-	sh acme.sh/acme.sh --issue -d $dominio -d /home/$ftpuser/public_html --server https://acme-v02.api.letsencrypt.org/directory
+git clone https://github.com/Neilpang/acme.sh.git
+sh acme.sh/acme.sh --set-default-ca --server letsencrypt
+sh acme.sh/acme.sh --issue -d $dominio -d www.$dominio -w /home/$ftpuser/public_html --server https://acme-v02.api.letsencrypt.org/directory
+else
+git clone https://github.com/Neilpang/acme.sh.git
+sh acme.sh/acme.sh --set-default-ca --server letsencrypt
+sh acme.sh/acme.sh --issue -d $dominio -w /home/$ftpuser/public_html --server https://acme-v02.api.letsencrypt.org/directory
 fi
 echo ""
 echo "Link para validação:"
